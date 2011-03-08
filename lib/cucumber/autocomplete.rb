45b2c36a2c59f5ba9a8997860c153c6099ac108a
julien <julien@lenovo-julien.(none)>
2011-03-08 09:52:15 +0100

    new cucumber step

--- lib/cucumber/autocomplete.rb
+++ lib/cucumber/autocomplete.rb
@@ -4,3 +4,7 @@ Given /^I choose "([^"]*)" in the autocomplete list$/ do |text|
   sleep 1
   page.execute_script %Q{ $('.ui-menu-item a:contains("#{text}")').trigger("mouseenter").trigger("click"); }
 end
+
+Then /^I should (see|not see) "([^"]+)" in the autocomplete list$/ do |see_or_not, text|
+  Then "I should #{see_or_not} \"#{text}\" within \"ul.ui-autocomplete.ui-menu\""
+end
